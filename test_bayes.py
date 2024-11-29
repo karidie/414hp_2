d83f0234578bd8148492f61cda0f7da669b1ac61
Yeongju Seong <yj2023@kentech.ac.kr>
2024-11-30 04:52:55 +0900

    aaaaaaa

---
+++ test_bayes.py
@@ -0,0 +1,202 @@
+# Naive Bayes On The Iris Dataset
+import sys
+from csv import reader
+from math import sqrt
+from math import exp
+from math import pi
+import logging
+import os
+import argparse
+
+# Load a CSV file
+def load_csv(filename):
+    dataset = list()
+    with open(filename, 'r') as file:
+        csv_reader = reader(file)
+        next(csv_reader)
+        for row in csv_reader:
+            if not row:
+                continue
+            dataset.append(row)
+    return dataset
+
+# Convert string column to float
+def str_column_to_float(dataset, column):
+    for row in dataset:
+        row[column] = float(row[column].strip())
+
+# Convert string column to integer
+def str_column_to_int(dataset, column):
+    class_values = [row[column] for row in dataset]
+    unique = set(class_values)
+    lookup = dict()
+    for i, value in enumerate(unique):
+        lookup[value] = i
+    for row in dataset:
+        row[column] = lookup[row[column]]
+    return lookup
+
+# Calculate accuracy percentage
+def accuracy_metric(actual, predicted):
+    correct = 0
+    for i in range(len(actual)):
+        if actual[i] == predicted[i]:
+            correct += 1
+    return correct / float(len(actual)) * 100.0
+
+
+# Split the dataset by class values, returns a dictionary
+def separate_by_class(dataset):
+    separated = dict()
+    for i in range(len(dataset)):
+        vector = dataset[i]
+        class_value = vector[-1]
+        if class_value not in separated:
+            separated[class_value] = list()
+        separated[class_value].append(vector)
+    return separated
+
+# Calculate the mean of a list of numbers
+def mean(numbers):
+    return sum(numbers) / float(len(numbers))
+
+# Calculate the standard deviation of a list of numbers
+def stdev(numbers):
+    avg = mean(numbers)
+    variance = sum([(x - avg) ** 2 for x in numbers]) / float(len(numbers) - 1)
+    return sqrt(variance)
+
+# Calculate the mean, stdev and count for each column in a dataset
+def summarize_dataset(dataset):
+    summaries = [(mean(column), stdev(column), len(column)) for column in zip(*dataset)]
+    del summaries[-1]
+    return summaries
+
+# Split dataset by class then calculate statistics for each row
+def summarize_by_class(dataset):
+    separated = separate_by_class(dataset)
+    summaries = dict()
+    for class_value, rows in separated.items():
+        summaries[class_value] = summarize_dataset(rows)
+    return summaries
+
+# Calculate the Gaussian probability distribution function for x
+def calculate_probability(x, mean, stdev):
+    exponent = exp(-((x - mean) ** 2 / (2 * stdev ** 2)))
+    return (1 / (sqrt(2 * pi) * stdev)) * exponent
+
+# Calculate the probabilities of predicting each class for a given row
+def calculate_class_probabilities(summaries, row):
+    total_rows = sum([summaries[label][0][2] for label in summaries])
+    probabilities = dict()
+    for class_value, class_summaries in summaries.items():
+        probabilities[class_value] = summaries[class_value][0][2] / float(total_rows)
+        for i in range(len(class_summaries)):
+            mean, stdev, _ = class_summaries[i]
+            probabilities[class_value] *= calculate_probability(row[i], mean, stdev)
+    return probabilities
+
+# Predict the class for a given row
+def predict(summaries, row):
+    probabilities = calculate_class_probabilities(summaries, row)
+    best_label, best_prob = None, -1
+    for class_value, probability in probabilities.items():
+        if best_label is None or probability > best_prob:
+            best_prob = probability
+            best_label = class_value
+    return best_label
+
+def report(predictions, answers):
+    if len(predictions) != len(answers):
+        logging.error("The lengths of two arguments should be same")
+        sys.exit(1)
+
+    # accuracy
+    correct = 0
+    for idx in range(len(predictions)):
+        if predictions[idx] == answers[idx]:
+            correct += 1
+    accuracy = round(correct / len(answers), 2) * 100
+
+    # precision
+    tp = 0
+    fp = 0
+    for idx in range(len(predictions)):
+        if predictions[idx] == 1:
+            if answers[idx] == 1:
+                tp += 1
+            else:
+                fp += 1
+    precision = round(tp / (tp + fp), 2) * 100
+
+    # recall
+    tp = 0
+    fn = 0
+    for idx in range(len(answers)):
+        if answers[idx] == 1:
+            if predictions[idx] == 1:
+                tp += 1
+            else:
+                fn += 1
+    recall = round(tp / (tp + fn), 2) * 100
+
+    logging.info("accuracy: {}%".format(accuracy))
+    logging.info("precision: {}%".format(precision))
+    logging.info("recall: {}%".format(recall))
+
+# Naive Bayes Algorithm
+def naive_bayes(train, test):
+    summarize = summarize_by_class(train)
+    predictions = list()
+    for row in test:
+        output = predict(summarize, row)
+        predictions.append(output)
+    return predictions
+
+def run(train_file, test_file):
+    train_dataset = load_csv(train_file)
+    test_dataset = load_csv(test_file)
+    
+    for i in range(len(train_dataset[0]) - 1):
+        str_column_to_float(train_dataset, i)
+        str_column_to_float(test_dataset, i)
+
+    # convert class column to integers
+    str_column_to_int(train_dataset, len(train_dataset[0]) - 1)
+    str_column_to_int(test_dataset, len(test_dataset[0]) - 1)
+
+    # evaluate algorithm
+    predicted = naive_bayes(train_dataset, test_dataset)
+    actual = [row[-1] for row in test_dataset]
+
+    report(predicted, actual)
+
+def command_line_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--training", required=False, metavar="<training dataset path>", help="training dataset path", default="./dataset/train/training1.csv")
+    parser.add_argument("-u", "--testing", required=False, metavar="<testing dataset path>", help="testing dataset path", default="./dataset/test/testing1.csv")
+    parser.add_argument("-l", "--log", help="Log level (DEBUG/INFO/WARNING/ERROR/CRITICAL)", type=str, default="INFO")
+
+    args = parser.parse_args()
+    return args
+
+# summary = summarize_dataset(train_dataset)
+# print(summary)
+def main():
+    args = command_line_args()
+
+    logging.basicConfig(level=args.log)
+
+    if not os.path.exists(args.training):
+        logging.error("The training dataset does not exist: {}".format(args.training))
+        sys.exit(1)
+
+    if not os.path.exists(args.testing):
+        logging.error("The testing dataset does not exist: {}".format(args.testing))
+        sys.exit(1)
+
+    run(args.training, args.testing)
+
+
+if __name__ == "__main__":
+    main()
